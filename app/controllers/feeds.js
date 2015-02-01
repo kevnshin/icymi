0ac824edfb92592b32f792aab4f0cd4ad77c7adc
Kevin Shin <kevnshin@gmail.com>
2015-01-31 17:28:29 -1000

    Able to fetch IG data in a workable format

--- app/controllers/feeds.js
+++ app/controllers/feeds.js
@@ -1,4 +1,4 @@
-var https = require('https');
+var request = require('request');
 var auth = require('../controllers/auth.js');
 
 function home_page (req, res) {
@@ -7,22 +7,29 @@ function home_page (req, res) {
 
 
 function view_instagram (req, res) {
+  var url = "https://api.instagram.com/v1/users/self/feed?access_token=";
 
-  console.log("req.user", req.user);
+  request( url + req.user.token.accesstoken + "&count=30", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log("body", JSON.parse(body).data.length);
+      // console.log("response",response);
+      var photos = JSON.parse(body).data;
+      // console.log(JSON.parse(body).data[0].likes);
+      var photos_data = [];
+      for (var i = 0; i < photos.length; i++) {
+        var photo_data = {
+          likes: photos[i].likes.count,
+          url: photos[i].link
+        }
+        photos_data.push(photo_data);
+      };
 
-  https.get("https://api.instagram.com/v1/users/self/feed?access_token=" + req.user.token.accesstoken, function(resp) {
-    // console.log("headers: ", resp.headers);
-
-    resp.on('data', function(d) {
-      process.stdout.write(d);
-    });
-
-    res.render('instagram_feed');
-
-  }).on('error', function(e) {
-    console.error(e);
-  });
-
+      console.log(photos_data);
+      res.render('instagram_feed');
+    } else {
+      res.send("Oops, it looks like you BROKE OUR WEBSITE!");
+    }
+  })
 }
 
 module.exports = {
